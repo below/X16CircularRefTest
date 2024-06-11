d53b4f1294e8a313392697e420dfe122e4b2d210
Alexander von Below <alex@vonbelow.com>
2024-06-11 10:35:26 +0200

    Initial Commit

---
+++ X16CircularRefTest/X16CircularRefTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  X16CircularRefTestApp.swift
+//  X16CircularRefTest
+//
+//  Created by Alexander von Below on 11.06.24.
+//
+
+import SwiftUI
+
+@main
+struct X16CircularRefTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
